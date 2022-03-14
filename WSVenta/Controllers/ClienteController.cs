c0b9b36c85311be515f22759ed3376d8ebe236d0
Alan <alanh1213@hotmail.com>
2022-03-14 04:26:11 -0300

    Agregar archivos de proyecto.

---
+++ WSVenta/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSVenta.Models;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (VentaRealContext db = new VentaRealContext())
+            {
+                var lst = db.Clientes.ToList();
+                return Ok(lst);
+            }
+
+            
+        }
+    }
+}
---
+++ WSVenta/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSVenta.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> lst = new List<WeatherForecast>();
+            lst.Add(new WeatherForecast() { Id = 5, Nombre = "Hector" });
+            lst.Add(new WeatherForecast() { Id = 6, Nombre = "Alan" });
+
+            return lst;
+        }
+    }
+}
