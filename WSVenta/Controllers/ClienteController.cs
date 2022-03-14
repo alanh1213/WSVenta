cb346b5dce976a9a9da08a118d45b894673b614b
Alan <alanh1213@hotmail.com>
2022-03-14 17:03:34 -0300

    Agregado CRUD con entity framework a tabla clientes

--- WSVenta/Controllers/ClienteController.cs
+++ WSVenta/Controllers/ClienteController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WSVenta.Models;
+using WSVenta.Models.Response;
+using WSVenta.Models.Request;
 
 namespace WSVenta.Controllers
 {
@@ -15,13 +17,102 @@ namespace WSVenta.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            using (VentaRealContext db = new VentaRealContext())
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+            try
             {
-                var lst = db.Clientes.ToList();
-                return Ok(lst);
+                
+
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    var lst = db.Clientes.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lst;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
             }
 
-            
+            return Ok(oRespuesta);
+
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClienteRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = new Cliente();
+                    oCliente.Nombre = oModel.Nombre;  //Mapea los campos del post(oModel) con los de la tabla(oCliente)
+                    db.Clientes.Add(oCliente); //Agrega el campo a la base de datos a la espera de confirmacion
+                    db.SaveChanges(); //Confirma los cambios
+                }
+                oRespuesta.Exito = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Update(ClienteRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = db.Clientes.Find(oModel.Id);  //Busca en la tabla Clientes el objeto por el ID
+                    oCliente.Nombre = oModel.Nombre;  //Mapea los campos del put(oModel) con los de la tabla(oCliente)
+                    db.Entry(oCliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified; 
+                    db.SaveChanges(); //Confirma los cambios
+                }
+                oRespuesta.Exito = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = db.Clientes.Find(Id);  //Busca en la tabla Clientes el objeto por el ID
+                    db.Remove(oCliente);
+                    db.SaveChanges(); //Confirma los cambios
+                }
+                oRespuesta.Exito = 1;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
         }
     }
 }
---
+++ WSVenta/Models/Response/Respuesta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSVenta.Models.Response
+{
+    public class Respuesta
+    {
+        //Molde de todas las solicitudes
+        public int Exito { get; set; }
+        public string Mensaje { get; set; }
+        public object Data { get; set; }
+    }
+}
